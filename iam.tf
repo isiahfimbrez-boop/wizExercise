1b696bb22a59c6e7cfc0f4a9ace8e801ff58f60c
Isiah Fimbrez <isiahfimbrez@Isiahs-MacBook-Pro.local>
2025-08-21 20:56:21 -0700

    Add in correct line for policy filepath1

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "aws_load_balancer_controller" {
   name        = "AWSLoadBalancerControllerIAMPolicy-Terraform"
   description = "IAM policy for AWS Load Balancer Controller"
-  policy      = file("/Users/isiahfimbrez/downloads/AWSproject/infrastructure/wizexerciseBackup/wizExerciseiam_policy2.json")
+  policy      = file("/Users/isiahfimbrez/downloads/AWSproject/infrastructure/wizexerciseBackup/wizExercise/iam_policy2.json")
 }
